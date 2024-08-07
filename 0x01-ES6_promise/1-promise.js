d4d03d80b04091b882802ac9f121c546f4737ca2
ojijoebrain <ojijosephoji@gmail.com>
2024-08-07 12:20:18 +0000

    1-promise.js

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,14 @@
+export default function (boolean) {
+  return new Promise((resolve, reject) => {
+    const object = {
+      status: 200,
+      body: 'Success',
+    };
+
+    if (boolean === true) {
+      resolve(object);
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
