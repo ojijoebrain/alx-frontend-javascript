d1d69e6015df93e1853b53c95de32bb1e16ee056
ojijoebrain <ojijosephoji@gmail.com>
2024-08-09 04:03:49 +0000

    100-evcar_update

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,7 +1,12 @@
-import Car from './10-car.js';
+import Car from './10-car';
 
 class EVCar extends Car {
-  constructor({ brand = '', motor = '', color = '', range = '' } = {}) {
+  constructor({
+    brand = '',
+    motor = '',
+    color = '',
+    range = '',
+  } = {}) {
     super(brand, motor, color);
     this._range = range;
   }
