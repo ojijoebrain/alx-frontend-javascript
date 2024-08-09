90d9081c801ba0d70e7e1b19cd1ef154104f8d6c
ojijoebrain <ojijosephoji@gmail.com>
2024-08-09 01:52:41 +0000

    3-currency

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,26 @@
+import Car from './10-car.js';
+
+class EVCar extends Car {
+  constructor({ brand = '', motor = '', color = '', range = '' } = {}) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  set range(value) {
+    if (typeof value === 'string' && value.length > 0) {
+      this._range = value;
+    } else {
+      throw new TypeError('Range must be a non-empty string');
+    }
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+}
+
+export default EVCar;
