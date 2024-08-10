d9905f70b0ec089389e8c21ffe1e942472f9b946
ojijoebrain <ojijosephoji@gmail.com>
2024-08-10 01:20:01 +0000

    100-evcar_udated

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,30 +1,13 @@
 import Car from './10-car';
 
 class EVCar extends Car {
-  constructor({
-    brand = '',
-    motor = '',
-    color = '',
-    range = '',
-  } = {}) {
+  constructor(brand = '', motor = '', color = '', range = '') {
     super(brand, motor, color);
     this._range = range;
   }
 
-  get range() {
-    return this._range;
-  }
-
-  set range(value) {
-    if (typeof value === 'string' && value.length > 0) {
-      this._range = value;
-    } else {
-      throw new TypeError('Range must be a non-empty string');
-    }
-  }
-
   static get [Symbol.species]() {
-    return this;
+    return Car;
   }
 }
 
