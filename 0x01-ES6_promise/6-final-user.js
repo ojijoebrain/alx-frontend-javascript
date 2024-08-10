0b695244be1a28e1ae72a411d53bbb7412c16b65
ojijoebrain <ojijosephoji@gmail.com>
2024-08-10 01:44:55 +0000

    -final_update_again

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,11 +5,10 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]).then((values) => {
-    const arr = [];
-    for (const item of values) {
-      arr.push({ status: item.status, value: item.value || item.reason });
-    }
-    return arr;
+  ]).then((results) => {
+    return results.map((result) => ({
+      status: result.status,
+      value: result.value || result.reason,
+    }));
   });
 }
