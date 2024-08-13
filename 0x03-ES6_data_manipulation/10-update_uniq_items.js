23a5adafd683b5fbc2dd1418a28a0a028f70fd6c
ojijoebrain <ojijosephoji@gmail.com>
2024-08-13 03:28:12 +0000

    10-update_uniq

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,13 @@
+export default function updateItemsInMap(map) {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
+  for (const [key, value] of map) {
+    if (value === 1) {
+      map.set(key, 100);
+    }
+  }
+
+  return map;
+}
